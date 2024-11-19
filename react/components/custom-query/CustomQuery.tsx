7e3a76a90939f6ae36513df1472e64709f1edc3d
unknown <caromorales1396@gmail.com>
2024-11-19 16:37:27 -0500

    fix: GraphQL query attempt

---
+++ react/components/custom-query/CustomQuery.tsx
@@ -0,0 +1,23 @@
+// import { useQuery } from 'react-apollo'
+// import React, { useEffect } from 'react'
+import React from "react"
+
+// import GET_PRODUCT from '../GraphQl/getProduct.gql'
+
+const CustomQuery = () => {
+  // useEffect(() => {
+  //   const { data, loading, error } = useQuery(GET_PRODUCT, {
+  //     variables: {
+  //       identifier: { field: 'refId', value: 89139 },
+  //     },
+  //   })
+
+  //   console.log(data)
+  //   console.log(loading)
+  //   console.log(error)
+  // }, [])
+
+  return <h1>prueba</h1>
+}
+
+export default CustomQuery
